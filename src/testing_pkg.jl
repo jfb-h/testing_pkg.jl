bd5eee965eb7699f476d2a41258ee4e535135b9f
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2020-03-27 16:40:32 +0100

    add a simple function add2() and test it.

--- src/testing_pkg.jl
+++ src/testing_pkg.jl
@@ -2,4 +2,8 @@ module testing_pkg
 
 greet() = print("Hello World!")
 
+include("extra_file.jl")
+
+export add2
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using testing_pkg
 using Test
 
 @testset "testing_pkg.jl" begin
-    # Write your own tests here.
+    @test add2(2) == 4
+    @test add2(5) == 7
+    @test add2(1) == 3
 end
